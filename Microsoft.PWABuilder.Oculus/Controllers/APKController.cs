634e8346bbf8accd69f5d649965d3d88d2951a03
Amrutha Srinivasan <amsrin@microsoft.com>
2021-11-17 17:35:54 -0800

    Added manifest fields

--- Microsoft.PWABuilder.Oculus/Controllers/APKController.cs
+++ Microsoft.PWABuilder.Oculus/Controllers/APKController.cs
@@ -7,7 +7,6 @@ namespace Microsoft.PWABuilder.Oculus.Controllers
     [Route("[controller]")]
     public class APKController : ControllerBase
     {
-        //private readonly ILogger<APKController> logger;
         [HttpPost]
         public string GenerateZip(OculusAppPackageOptions options)
         {
@@ -16,8 +15,6 @@ namespace Microsoft.PWABuilder.Oculus.Controllers
 
         private string CreateAppPackage(OculusAppPackageOptions options)
         {
-            //logger.LogInformation("Generating app package for {url} using {options}", options.Url, options);
-
             return "This API was called";
         }
     }
--- Microsoft.PWABuilder.Oculus/Models/OculusAppPackageOptions.cs
+++ Microsoft.PWABuilder.Oculus/Models/OculusAppPackageOptions.cs
@@ -2,30 +2,41 @@
 {
     public class OculusAppPackageOptions
     {
-        /// <summary>
-        /// The package ID that uniquely identifies the app. Can only contain letters, numbers, hypen, and period. Typically, "MyCompany.MyApp". If unspecified, one will be created based on the URL.
-        /// </summary>
-        public string PackageId { get; set; } = string.Empty;
 
         /// <summary>
-        /// Gets the display name of the app. If not supplied, the name will be fetched from the web manifest.
+        /// Represents the name of the web application as it is usually displayed to the user
         /// </summary>
-        public string? Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         /// <summary>
-        /// The URL of the PWA to generate an .msix file from.
+        /// Determines the developers’ preferred display mode for the website. Only "standalone" or "minimal-ui" are accepted. (Setting standalone as default)
         /// </summary>
-        public string Url { get; set; } = string.Empty;
+        public string Display { get; set; } = "standalone";
 
         /// <summary>
-        /// The version of the app.
+        /// Represents the name of the web application displayed to the user if there is not enough space to display name.
         /// </summary>
-        public string Version { get; set; } = string.Empty;
+        public string? ShortName { get; set; }
 
         /// <summary>
-        /// Whether the generated MSIX package will be signable. If false, the app won't be able to be submitted to the Store.
+        /// Defines the navigation scope of this web application's application context. It restricts what web pages can be viewed while the manifest is applied. If the user navigates outside the scope, it reverts to a normal web page inside a browser tab or window.
         /// </summary>
-        public bool AllowSigning { get; set; }
+        public string? Scope { get; set; }
+
+        /// <summary>
+        /// Represents the start URL of the web application — the preferred URL that should be loaded when the user launches the web application.
+        /// </summary>
+        public string? StartUrl { get; set; }
+
+        /// <summary>
+        /// If true, this boolean field will give your PWA a tab bar similar to Oculus Browser
+        /// </summary>
+        public bool? OvrMultiTabEnabled { get; set; }
+
+        /// <summary>
+        /// Allows your PWA to include more web pages within the scope of your web application. Consists of a JSON dictionary containing extension URLs or wildcard patterns.
+        /// </summary>
+        public string? OvrScopeExtensions { get; set; }
 
     }
 }
